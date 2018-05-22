522edde750acad570f6c8ea6028152d0546a66ae
nwoodruff149 <35577657+nwoodruff149@users.noreply.github.com>
2018-05-22 19:14:13 +0100

    Switched to ExodusUI and made createLabel method

--- Exodus/src/exodus/ExodusUI.java
+++ Exodus/src/exodus/ExodusUI.java
@@ -6,6 +6,8 @@
 package exodus;
 
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,28 +17,51 @@ import javax.swing.JPanel;
  * @author 12nwoodruff
  */
 public class ExodusUI {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         ExodusUI ui = new ExodusUI();
-    }
+    }*/
     public ExodusUI()
     {
         JFrame jf = new JFrame();
-        JPanel jp = new JPanel();
         
+        JLabel localMenu = createLabel(0, 500, 400, 580, Color.GRAY, null);
+        JLabel worldMenu = createLabel(1520, 500, 400, 580, Color.GRAY, null);
+        
+//        jf.add(localMenu);
+//        jf.add(worldMenu);
         jf.setSize(1920, 1080);
+        jf.setLayout(null);
         jf.setExtendedState(JFrame.MAXIMIZED_BOTH);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setUndecorated(true);
-        
-        JLabel localMenu = new JLabel();
-        localMenu.setBackground(Color.RED);
-        localMenu.setLocation(0, 580);
-        localMenu.setSize(300, 500);
-        localMenu.setVisible(true);
-        
-        jp.add(localMenu);
-        jf.add(jp);
         jf.revalidate();
         jf.setVisible(true);
+    }
+    
+    public JLabel createLabel(int x, int y, int width, int height, Object background, String text)
+    {
+        if(background.getClass() != Color.class && background.getClass() != ImageIcon.class)
+        {
+            return null;
+        }
+        JLabel label = new JLabel();
+        label.setText(text);
+        label.setLocation(x, y);
+        label.setSize(width, height);
+        if(background.getClass() == Color.class)
+        {
+            label.setBackground((Color) background);
+        }
+        else
+        {
+            label.setIcon((ImageIcon) background);
+        }
+        label.setVisible(true);
+        label.setOpaque(true);
+        return label;
+    }
+    
+    public static void main(String[] args) {
+        ExodusUI ui = new ExodusUI();
     }
 }
